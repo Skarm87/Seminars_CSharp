36ee5aa760462407be22e2e02ac06a5d826d9373
Skarm87 <skarm87@yandex.ru>
2024-10-02 18:24:04 +0400

    added fixing

--- Sem1Task3/Program.cs
+++ Sem1Task3/Program.cs
@@ -7,9 +7,15 @@
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int firstDigit = num / 100; // 456 / 10=4.56
-int lastDigit = num % 10; // 456 % 10=450+6
 
-int result = firstDigit + lastDigit;
-
-Console.WriteLine($"Сумма первой и последней цифры: {result}");
+if (num >= 100 && num <= 999) // if (num > 99 && num < 1000)
+{
+    int firstDigit = num / 100; // 456 / 10=4.56
+    int lastDigit = num % 10; // 456 % 10=450+6
+    int result = firstDigit + lastDigit;
+    Console.WriteLine($"Сумма первой и последней цифры: {result}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода: число не является трехзначным.");
+}
