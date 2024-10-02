17e577f8cda4de37c62f0dbb2a01a7ed88bd5504
Skarm87 <skarm87@yandex.ru>
2024-10-02 17:45:39 +0400

    fixing code

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -7,10 +7,17 @@
 Console.WriteLine("Введите целое положительное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int count = -num; // num * -1
-
-while (count <= num)
+if (num > 0)
 {
-    Console.Write (count + " ");
-    count++; // count = count + 1; count += 1;
+    int count = -num; // num * -1
+
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++; // count = count + 1; count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
 }
