9687fe1649db423c827dbd90b710ec167f57e792
Skarm87 <skarm87@yandex.ru>
2024-10-08 17:27:30 +0400

    added HW 1-4

---
+++ Sem2_HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+using System;
+class Program
+
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("Да.");
+        }
+        else
+        {
+            Console.WriteLine("Нет.");
+        }
+    }
+}
