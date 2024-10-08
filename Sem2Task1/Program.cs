294511d20c8a0877c0849ec5aa6c27670bbc6555
Skarm87 <skarm87@yandex.ru>
2024-10-08 14:58:20 +0400

    added Task1 & Task2

---
+++ Sem2Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и удаляет вторую цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+
+Console.Write("Введите целое положительное трёхзначное число: ");
+
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100; // 256 / 10 = 2.56 = 2
+    int thirdDigit = num % 10; // 256 % 10 = 250 + 6 = 6
+
+    int result = firstDigit * 10 + thirdDigit;
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
---
+++ Sem2Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и возводит вторую цифру этого числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 = 5^4 = 625
+// 617 => 1
+
+
+
+Console.WriteLine("Введите трёхначное целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = num / 10 % 10;
+int lastDigit = num % 10;
+
+int result = secondDigit;
+int count = 1;
+
+while (count < lastDigit)
+{
+    result = result * secondDigit; // result *= secondDigit
+    count++;
+}
+
+Console.WriteLine(result);
